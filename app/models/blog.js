a08d3ec80f7bb2e4bcb46875e7c31e2c54badd62
Julie Heidt <julie.a.heidt@gmail.com>
2015-09-26 14:55:39 -0700

    build Blog model + blog.js

--- app/models/blog.js
+++ app/models/blog.js
@@ -1,1 +1,17 @@
-var 
+//Blog Model
+var mongoose = require( 'mongoose' ),
+	Schema = mongoose.Schema;
+
+var BlogSchema = new mongoose.Schema( {
+	title: String,
+	author: String,
+	created_at: Date,
+	content: String,
+});
+
+BlogSchema.pre('save', function(next) {
+	this.created_at = new Date();
+	next();
+});
+
+module.exports = mongoose.model( 'Blog', BlogSchema );
